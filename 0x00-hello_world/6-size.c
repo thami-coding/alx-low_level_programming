f810fa631912d45c05e33f5470c4fe02e9e7c09d
thami <sainttsquared@gmail.com>
2022-06-30 11:11:31 -0700

    modified 6-size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n",
 	(unsigned long) sizeof(long long int));
-	printf("Size of a float: %lu byte(s)", (unsigned long) sizeof(float));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long) sizeof(float));
 	return (0);
 }
