620249f4872564cd5e78f8615f3a67d75ebb49bb
thami <sainttsquared@gmail.com>
2022-07-07 17:11:40 -0700

    added new file 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+ * print_square -  a function that prints a square
+ * @size: size is the size of the square
+ * Return: void
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
