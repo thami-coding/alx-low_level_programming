716a6ac3da8f8ff10246f6bdc30a8c356a0de831
thami <sainttsquared@gmail.com>
2022-07-11 10:49:38 -0700

    added new file 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen -  a function that returns the length of a string
+ * @s: string to check length
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	return (i);
+}
