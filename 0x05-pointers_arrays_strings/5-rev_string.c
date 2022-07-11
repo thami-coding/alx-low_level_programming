bd5e692b9e63998ddae701c2c2730472daa82d25
thami <sainttsquared@gmail.com>
2022-07-11 15:45:57 -0700

    added new file 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - a function that reverses a string
+ * @s: string to reverse
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, j, k, l;
+
+	char rev[200];
+
+	k = 0;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		rev[k] = s[j];
+		k++;
+	}
+	for (l = 0; s[l] != 0; l++)
+	{
+		s[l] = rev[l];
+	}
+}
