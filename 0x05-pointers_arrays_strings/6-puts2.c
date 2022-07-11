8e75cb41c21e0954665f3f77b8883567f6ddd7db
thami <sainttsquared@gmail.com>
2022-07-11 16:17:06 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,11 +6,15 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; str[i] != 0; i += 2)
+	for (i = 0; str[i] != 0; i++)
 	{
-		_putchar(str[i]);
+	}
+
+	for (j = 0; j < i; j += 2)
+	{
+		_putchar(str[j]);
 	}
 	_putchar('\n');
 }
