dd94778c276fe96c07e8eee9eebdd8b8acaea693
thami <sainttsquared@gmail.com>
2022-07-14 10:23:33 -0700

    modified:   3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,7 @@
  * @s2: string to compare
  * Return: 0 if strings are the same, -15 if s1 >s2 otherwise 15
  */
-int _strcmp(char *s1, char *s2)
+int _strcmp(char* s1, char* s2)
 {
 	int i, j, k;
 
@@ -15,25 +15,22 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] != 0)
 		i++;
 
-	while(s2[j] != 0)
+	while (s2[j] != 0)
 		j++;
 
-	for (k =0; k < i; k++)
+	for (k = 0; k < i; k++)
 	{
 		if (s1[k] == s2[k])
 		{
 			if (k == i - 1)
-			{
 				return 0;
-			}
 			continue;
 		}
 		break;
 	}
 
 	if (i < j)
-		return -15;
+		return (-15);
 
-	if(i > j)
-		return 15;
+	return (15);
 }
