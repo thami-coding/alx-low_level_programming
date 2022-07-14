dd147f293dd32be7128e20996d44eeac040d70cd
thami <sainttsquared@gmail.com>
2022-07-13 22:04:18 -0700

    modified:   2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,8 +11,12 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; i < n; i++)
 	{
+		if (i >= 47)
+		{
+			dest[i] = 0;
+			continue;
+		}
 		dest[i] = src[i];
 	}
-
 	return (dest);
 }
